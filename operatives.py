ffe4ab5336e2020d8a663af5cd5dc88312022012
aryam003 <aryamarya537@gmail.com>
2024-08-13 13:31:13 +0530

    python

---
+++ operatives.py
@@ -0,0 +1,56 @@
+# print(10+4)
+# print(100-10)
+# print(10/10)
+# print(10*8)
+# print('ha'*20)
+# print(20//10)
+# print(10*2)
+# print(10%3)
+# print('sree'+'laya')
+
+
+# a=30
+# a+=5
+# print(a)
+
+
+# a=30
+# a-=10
+# print(a)
+
+# a=30
+# a/=2
+# print(a)
+
+# a=40
+# a*=2
+# print(a)
+
+# a=20
+# a**=2
+# print(a)
+
+# a=10
+# a%=3
+# print(a)
+
+# a='hh'
+# a*=6
+# print(a)
+
+
+print(10==10)
+
+print(10==5)
+
+print(10!=13)
+print(10!=10)
+
+print(10>5)
+print(10>15)
+
+print(10<20)
+print(10<5)
+
+print(10<=20)
+print(10<=10)
