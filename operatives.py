d0f3ffc2047f07fe0d17f26bc786485f13763100
aryam003 <aryamarya537@gmail.com>
2024-08-14 13:36:36 +0530

    python

--- operatives.py
+++ operatives.py
@@ -8,11 +8,9 @@
 # print(10%3)
 # print('sree'+'laya')
 
-
 # a=30
 # a+=5
 # print(a)
-
 
 # a=30
 # a-=10
@@ -38,19 +36,100 @@
 # a*=6
 # print(a)
 
+# print(10==10)
 
-print(10==10)
+# print(10==5)
 
-print(10==5)
+# print(10!=13)
+# print(10!=10)
 
-print(10!=13)
-print(10!=10)
+# print(10>5)
+# print(10>15)
 
-print(10>5)
-print(10>15)
+# print(10<20)
+# print(10<5)
 
-print(10<20)
-print(10<5)
+# print(10<=20)
+# print(10<=10)
 
-print(10<=20)
-print(10<=10)
+# print(not(10==10 and 10>=9))
+# print(10==9 and 10>=13)
+
+# print(10==10 or 10>=11)
+# print(10<=20 or 10>=13)
+
+# print(not(20==20))
+# print(not(10>=5))
+# print(10==10 and 10>=14)
+
+# a='arya'
+# l='y'
+# print(l in a)
+
+# a='arya'
+# l='m'
+# print(l in a)
+
+# a='arya'
+# b='s'
+# print(a not in b)
+
+# a='arya'
+# b='a'
+# print(b not in a)
+
+# l=[1,2,3,4]
+# print(2 in l)
+
+# l=[1,2,3,4]
+# print(2 not in l)
+
+a=20
+b=20
+print(id(a))
+print(id(b))
+print(a is b)
+
+a=20
+b=10
+print(id(a))
+print(id(b))
+print(a is b)
+
+l=[1,2,3,4]
+i=[1,2,3,4]
+print(id(l))
+print(id(i))
+print(l is i)
+
+a=20
+b=20
+print(id(a))
+print(id(b))
+print(a is not b)
+
+a=20
+r=20
+print(id(a))
+print(id(r))
+print(a is r)
+
+print('y' in 'arya' and 'r' in 'arya')
+
+a='anu'
+b='anu'
+print(id(a))
+print(id(b))
+print(a is b)
+
+a='anu'
+b='ammu'
+print(id(a))
+print(id(b))
+print(a is b)
+
+a='anu'
+b='anu'
+print(id(a))
+print(id(b))
+print(a is not b)
---
+++ string.py
@@ -0,0 +1,13 @@
+# print('python\n','java')
+
+# print('python\t','java')
+
+# a='sreelaya'
+# print(len(a))
+
+# b='cookie'
+# print(b[-1])
+
+a="""python
+php"""
+print(a)
