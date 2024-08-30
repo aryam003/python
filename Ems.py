8a033dab0750969a29050644f428fa9337adb300
aryam003 <aryamarya537@gmail.com>
2024-08-30 13:41:56 +0530

    list

---
+++ Ems.py
@@ -0,0 +1,20 @@
+Ems=[]
+while True:
+    print('''
+1.Register 
+2.View
+3.Undate
+4.Delet
+5.Search
+Add task
+''')
+    choice=int(input("Enter ur choice:"))
+    if choice==1:
+        id=int(input("enter id :"))
+        name=input("enter name :")
+        age=int(input("enter age :"))
+        salary=int(input("enter salary :"))
+        age=int(input("enter position :"))
+        age=int(input("enter experience :"))
+        
+        
--- list.py
+++ list.py
@@ -148,20 +148,54 @@ l1= [1, 2, 3, 4]'''
 '''[['Anu', 20, 90], ['Ammu', 15, 100]]'''
 std=[]
 while True:
-    print('''
+     print('''
 1.Add std
 2.View std
 3.Update std
-4.Delet std                           
+4.Delet std 
+5.Search std
+6.Exit                                                
 ''')
-    choice=int(input("enter ur choice"))
-    if choice==1:
+     choice=int(input("enter ur choice:"))
+     if choice==1:
          name=(input("enter a name:"))
          age=int(input("enter age:"))
          mark=int(input("enter mark:"))
          std.append([name,age,mark])
-    elif choice==2:
+     elif choice==2:
          print('{:<10}{:<5}{:<5}'.format('name','age','mark'))
          print('_'*20)
-    for i in std:
-         print('{:<10}{:<5}{:<5}'.format(i[0],i[1],i[2]))     
+         for i in std:
+               print('{:<10}{:<5}{:<5}'.format(i[0],i[1],i[2])) 
+     elif choice==3:
+          name=input("enter name:")
+          f=0
+          for i in std:
+               if i[0]==name:
+                    mark=int(input("enter new mark:"))
+                    i[2]=mark
+                    f=1
+          if f==0:
+               print("name not in list") 
+     elif choice==4:
+          name=input("enter name:")
+          f=0
+          for i in std:
+               if i[0]==name:
+                    std.remove(i)
+                    f=1
+          if f==0:
+               print("name not in list")
+     elif choice==5:
+          name=input("enter name:")
+          f=0
+          for i in std:
+               if i[0]==name:
+                    print(i)
+                    f=1
+          if f==0:
+               print("name not in list")
+     elif choice==6:
+          break
+     else:
+          print("invalid choice")                                     
