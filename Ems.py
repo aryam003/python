30bb48c150030b6684e07ef818074d0a8e0f1894
aryam003 <aryamarya537@gmail.com>
2024-08-31 13:37:24 +0530

    list

--- Ems.py
+++ Ems.py
@@ -1,3 +1,4 @@
+import datetime
 Ems=[]
 while True:
     print('''
@@ -7,6 +8,8 @@ while True:
 4.Delete
 5.Search
 6.Add task
+7.view task
+8.Exit                    
 ''')
     choice=int(input("Enter ur choice:"))
     if choice==1:
@@ -27,15 +30,30 @@ while True:
         f=0
         for i in Ems:
             if i[0]==id:
-                age=int(input("enter new age:"))
-                salary=int(input("enter new salary:"))
-                position=(input("enter new position:"))
-                experience=int(input("enter ur experience"))
-                i[2]=age
-                i[3]=salary
-                i[4]=position
-                i[5]=experience
-                f=1 
+                f=1
+                while True:
+                    print('''
+1.age
+2.salary
+3.position
+4.experience
+5.exit
+''')
+                    sub_choice=int(input("enter ur choice for undate:"))
+                    if sub_choice==1:
+                        new_age=int(input("new age:"))
+                        i[2]=new_age
+                    elif sub_choice==2:
+                        new_salary=int(input("enter new salary:"))
+                        i[3]=new_salary
+                    elif sub_choice==3:
+                        new_position=(input("enter new position:"))
+                        i[4]=new_position
+                    elif sub_choice==4:
+                        new_experience=int(input("enter ur experience:"))
+                        i[5]=new_experience
+                    elif sub_choice==5:
+                        break    
         if f==0:
             print("id nit in list")
     elif choice==4:
@@ -55,4 +73,28 @@ while True:
                 print(i)
                 f=1
         if f==0:
-            print("id not in list")                                               
+            print("id not in list")
+    elif choice==6:
+        id=int(input("enter id:"))
+        f=0
+        for i in Ems:
+            if i[0]==id:
+                f=1
+                task=input("enter task:")
+                date=datetime.datetime.now().strftime("%x")
+                i.append([task,date])
+        if f==0:
+            print("invalid id")
+    elif choice==7:
+        print('{:<5}{:<10}{:<10}{:<10}'.format('id','name','task','date'))    
+        print('_'*30)
+        for i in Ems:
+            if len(i)>6:
+               print('{:<5}{:<10}{:<10}{:<10}'.format(i[0],i[1],i[6][0],i[6][1]))
+    elif choice==8:
+          break
+    else:
+          print("invalid choice")            
+
+
+
