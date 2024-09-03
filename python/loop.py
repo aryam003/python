436c325f63f00417d250d099bac2f32b48aa28cc
aryam003 <aryamarya537@gmail.com>
2024-09-03 13:34:24 +0530

    python

---
+++ python/loop.py
@@ -0,0 +1,121 @@
+# i=1
+# while i<=5:
+#     print(i)
+#     i+=1
+
+# i=int(input("enter starting number"))
+# a=int(input("enter ending number"))
+# while i<=a:
+#     print(i)
+#     i+=5
+# i=1
+# a=int(input("enter limit:"))
+# while i<=a:
+#     print("python")
+#     i+=1
+
+# i=int(input("enter starting number:"))
+# e=int(input("enter ending number:"))
+# sum=0
+# while i<=e:
+#     sum=sum+i
+#     i+=1
+# print('sum :',sum)    
+
+i=int(input("enter starting number:"))
+e=int(input("enter ending number:"))
+while i<=e:
+    if i%2==0:
+        print(i)
+    i+=1
+    
+# i=int(input("enter starting number:"))
+# e=int(input("enter ending number:"))
+# while i<=e:
+#     if i%3==0:
+#         print(i)
+#     i+=1
+
+# i=int(input("enter starting number:"))
+# e=int(input("enter ending number:"))
+# sum=0
+# while i<=e:
+#     if i%2==0:
+#         sum+=i
+#     i+=1
+# print('sum :',sum)    
+
+# i=int(input("enter starting number:"))
+# e=int(input("enter ending number:"))
+# sum=0
+# while i<=e:
+#     if i%2!=0:
+#         sum+=i
+#     i+=1
+# print('sum :',sum)  
+#   
+# i=1
+# a=int(input("enter a number:"))
+# while i<=10:
+#     print(i,'*',a,'=',i*a)
+#     i+=1
+
+# i=int(input("enter starting number:"))
+# e=int(input("enter ending number:"))
+# sum=0
+# odd_sum=0
+# normal_sum=0
+# while i<=e:
+#     normal_sum+=i
+#     if i%2==0:     
+#         sum+=i
+#     else:
+#         odd_sum+=i
+#     i+=1
+# print('even sum :',sum) 
+# print('odd sum :',odd_sum) 
+# print('normal sum :',normal_sum) 
+
+# a=int(input("enter ending value:"))
+# i=1
+# factorial=1
+# while i<=a:
+#     factorial*=i
+#     i+=1
+# print('factorial:',factorial)    
+
+# a=int(input("enter a number :"))
+# rev=0
+# while a>0:
+#     d=a%10
+#     # print(d)
+#     rev=rev*10+d
+#     a//=10
+# print(rev)
+
+# a=int(input("enter a number :"))
+# rev=0
+# while a>0:
+#     d=a%10
+#     # print(d)
+#     rev=rev+d
+#     a//=10
+# print(rev)
+
+# a=input('str:')
+# l=len(a)
+# i=0
+# while i<l:
+#     print(a[i])
+#     i+=1
+   
+
+a=input("str:")
+l=len(a)
+i=0
+rev=''
+while i<l:
+    rev=a[i]+rev
+    i+=1
+print(rev)    
+
---
+++ python/operatives.py
@@ -0,0 +1,135 @@
+# print(10+4)
+# print(100-10)
+# print(10/10)
+# print(10*8)
+# print('ha'*20)
+# print(20//10)
+# print(10*2)
+# print(10%3)
+# print('sree'+'laya')
+
+# a=30
+# a+=5
+# print(a)
+
+# a=30
+# a-=10
+# print(a)
+
+# a=30
+# a/=2
+# print(a)
+
+# a=40
+# a*=2
+# print(a)
+
+# a=20
+# a**=2
+# print(a)
+
+# a=10
+# a%=3
+# print(a)
+
+# a='hh'
+# a*=6
+# print(a)
+
+# print(10==10)
+
+# print(10==5)
+
+# print(10!=13)
+# print(10!=10)
+
+# print(10>5)
+# print(10>15)
+
+# print(10<20)
+# print(10<5)
+
+# print(10<=20)
+# print(10<=10)
+
+# print(not(10==10 and 10>=9))
+# print(10==9 and 10>=13)
+
+# print(10==10 or 10>=11)
+# print(10<=20 or 10>=13)
+
+# print(not(20==20))
+# print(not(10>=5))
+# print(10==10 and 10>=14)
+
+# a='arya'
+# l='y'
+# print(l in a)
+
+# a='arya'
+# l='m'
+# print(l in a)
+
+# a='arya'
+# b='s'
+# print(a not in b)
+
+# a='arya'
+# b='a'
+# print(b not in a)
+
+# l=[1,2,3,4]
+# print(2 in l)
+
+# l=[1,2,3,4]
+# print(2 not in l)
+
+a=20
+b=20
+print(id(a))
+print(id(b))
+print(a is b)
+
+a=20
+b=10
+print(id(a))
+print(id(b))
+print(a is b)
+
+l=[1,2,3,4]
+i=[1,2,3,4]
+print(id(l))
+print(id(i))
+print(l is i)
+
+a=20
+b=20
+print(id(a))
+print(id(b))
+print(a is not b)
+
+a=20
+r=20
+print(id(a))
+print(id(r))
+print(a is r)
+
+print('y' in 'arya' and 'r' in 'arya')
+
+a='anu'
+b='anu'
+print(id(a))
+print(id(b))
+print(a is b)
+
+a='anu'
+b='ammu'
+print(id(a))
+print(id(b))
+print(a is b)
+
+a='anu'
+b='anu'
+print(id(a))
+print(id(b))
+print(a is not b)
