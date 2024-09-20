c8bf7198593da4ba24bf8b3af1934ae2d6fb6d6c
aryama003 <aryamarya537@gmail.com>
2024-09-20 13:42:21 +0530

    work

--- l.py
+++ l.py
@@ -1,5 +1,5 @@
-user=[]
-book=[]
+user=[{'id':101,'name':'anu','address':'w','email':'anu@gmail.com','phone':22222,'password':'ww','books':[]}]
+book=[{'id':101,'name':'name','stock':2,'price':2}]
 def add():
     if len(book)==0:
         id=101
@@ -38,23 +38,23 @@ def delete():
             book.remove(i)
     if f1==0:
         print("Invalid id.") 
-# def view_user():
-#     print('{:<10}{:<10}{:<10}{:<10}'.format('id','name','address','phone','password'))
-#     print('_'*50)
-#     for i in user:
-#         print('{:<10}{:<10}{:<10}{:<10}{:<10}'.format(i['id'],i['name'],i['address'],i['phone'],i['password']))                       
+def view_user():
+    print('{:<10}{:<10}{:<10}{:<10}{:<10}'.format('id','name','address','phone','email'))
+    print('_'*50)
+    for i in user:
+        print('{:<10}{:<10}{:<10}{:<10}{:<10}'.format(i['id'],i['name'],i['address'],i['phone'],i['email']))                       
 def login():
     u_name=input("Enter name:")
     p_word=input("Enter password:")
     f=0
-    user=''
+    u=''
     if u_name=='admin' and p_word=='admin':
         f=1
     for i in user:
         if i['email']==u_name and i['password']==p_word:
             f=2
-            user=i
-    return f,user          
+            u=i
+    return f,u          
 def register():
     if len(user)==0:
         id=101
@@ -73,7 +73,26 @@ def register():
         address=input('Enter address:')
         phone=int(input('Enter phone No:'))
         password=input("Enter password:")
-        user.append({'id':id,'name':name,'address':address,'email':email,'phone':phone,'password':password})
+        user.append({'id':id,'name':name,'address':address,'email':email,'phone':phone,'password':password,'books':[]})
+def view_profile(user):
+    print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format('id','name','address','phone','email','password'))
+    print('_'*60)
+    print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format(user['id'],user['name'],user['address'],user['phone'],user['email'],user['password']))
+def borrow_book(user):
+    id=int(input("Enter id:"))
+    f=0
+    for i in book:
+        if i['id']==id:
+            f=1
+            if i['stock']>0:
+                user['books'].append(i['id'])
+                i['stock']-=1
+                print('book added')
+            else:
+                print("outof stock.")
+    if f==0:
+        print('invalid id')
+ 
 while True:
     print('''
 1.Register
@@ -84,7 +103,7 @@ while True:
     if choice==1:
         register()
     elif choice==2:
-        f,user=login()
+        f,u=login()
         if f==1:
             while True:
                 print('''
@@ -104,12 +123,29 @@ while True:
                     undate()
                 elif sub_ch==4:
                     delete() 
-                # elif sub_ch==5:
-                #     view_user()  
+                elif sub_ch==5:
+                    view_user()  
                 elif sub_ch==6:
                     break      
         elif f==2:
-            print('user login')
+            while True:
+                print('''
+1.View profile.
+2.View book.
+3.Borrow book.
+4.Return book.
+5.Book in hand.
+6.Exit.
+''')
+                sub_ch=int(input("enter ur choice:"))
+                if sub_ch==1:
+                    view_profile(u)
+                elif sub_ch==2:
+                    view()    
+                elif sub_ch==3:
+                    borrow_book(u)
+                # elif sub_ch==4:
+                    
         else:
             print('Invalid username or password.')
             
