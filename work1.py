e939295f7bbce2af832abd007fa0ca834fe67d58
aryam003 <aryamarya537@gmail.com>
2024-09-25 13:35:38 +0530

    file_work1

--- work1.py
+++ work1.py
@@ -33,10 +33,10 @@
 # print(a)
 '''hello(demo.txt)'''
 
-# f=open('Python/demo.txt','x')
+# f=open('python/demo.txt','x')
 # f.write('welcome \n hello \n hi')
 
-# f=open('Python/demo.txt','r')
+# f=open('python/demo.txt','r')
 # a=f.read()
 # print(a)
 '''Welcome'''
@@ -47,7 +47,7 @@
 
 
 
-# f=open('Python/demo.txt','r')
+# f=open('python/demo.txt','r')
 # a=f.read(5)
 
 # f.seek(0)
@@ -66,7 +66,7 @@ ____________________
 welcome
 '''
 
-# f=open('Python/demo.txt','r')
+# f=open('python/demo.txt','r')
 # a=f.readline(5)
 # b=f.readline()
 # c=f.readline()
@@ -81,7 +81,7 @@ hello
 
 '''
 
-# f=open('Python/demo.txt','r')
+# f=open('python/demo.txt','r')
 # a=f.readline(5)
 # b=f.readline()
 
@@ -99,7 +99,7 @@ me
 
 
 
-# f=open('Python/demo.txt','r')
+# f=open('python/demo.txt','r')
 # l=f.readlines()
 # f.seek(0)
 # for i in range(len(l)):
@@ -112,7 +112,7 @@ hello
 hi
 '''   
 
-# f=open('Python/demo.txt','r')
+# f=open('python/demo.txt','r')
 # l=f.readlines()
 # f.seek(0)
 
@@ -122,61 +122,111 @@ hi
 #     rev=i+rev
     
 # print(rev) 
-
 '''
 welcome 
 hellowelcome 
 hihellowelcome '''    
 
-# f=open('Python/demo.txt','r')
+# f=open('python/demo.txt','r')
 # l=f.readlines()
 # f.seek(0)
 # for i in range(len(l)):
 #     a=f.readline().strip()
 #     print(a[::-1])   # slicing method
-
 '''
 emoclew
 olleh
 ih
 '''
-
-# f=open('Python/demo1.txt','w')
+# f=open('python/demo1.txt','w')
 # f.write('\nWELCOME')
 
-# f=open('Python/demo1.txt','a')
+# f=open('python/demo1.txt','a')
 # f.write('\nWELCOME')
 '''
 WELCOME
 WELCOME
 WELCOME
 '''
-
-
     #  print(x*y)
 
-# f=open('Python/demo1.txt','w')
+# f=open('python/demo1.txt','w')
 # a=int(input("Enter a no :"))
 # for i in range(1,11):
 #     # print(i ,'*', a ,'=',i*a)
 #     f.write(str(i)+'*'+str(a)+'='+str(i*a)+'\n')
 '''write in to new file (1*2=2 to 10*2=20)'''
 
-# f=open('Python/demo2.txt','w')
+# f=open('python/demo2.txt','w')
 # a=int(input("Enter a no :"))
 # for i in range(1,11):
 #     # print(i ,'*', a ,'=',i*a)
 #     f.write(str(i)+'*'+str(a)+'='+str(i*a)+'\n')
 
-
-# f=open('Python/demo2.txt', 'w')
-# for a in range(1, 6):
-#     for i in range(1, 11):
-#         f.write(str(i)+'*'+str(a)+'='+str(i*a)+'\n')
         
 
-w=open('Python/demo2.txt', 'w')
-for i in range(1, 11):  
-        for a in range(1, 6):  
-            w.write(str(i)+'*'+str(a)+'='+str(i*a)+'\t')
-        w.write("\n")
+# w=open('python/demo2.txt', 'w')
+# for i in range(1, 11):  
+#         for a in range(1, 6):  
+#             w.write(str(i)+'*'+str(a)+'='+str(i*a)+'\t')
+#         w.write("\n")
+'''or'''
+# w=open('python/demo2.txt', 'w')
+# s=int(input("enter a No:"))
+# for i in range(1, 11):  
+#         for a in range(1,s+1):  
+#             w.write(str(i)+'*'+str(a)+'='+str(a*i)+'\t')
+#         w.write("\n")
+
+# f=open('python/demo.txt','r')
+# l=f.readlines()
+# f.seek(0)
+# letter=0
+# for i in range(len(l)):
+#     a=f.readline().strip()
+#     for i in a:
+#         if i !=' ':
+#             letter+=1
+# print(letter)
+'''14'''
+
+# f=open('./arya/python/demo.txt','r')
+# l=f.readlines()
+# f.seek(0)
+# letter=0
+# cap=0
+# for i in range(len(l)):
+#     a=f.readline().strip()
+#     for i in a:
+#          if i !=' ':
+#             if i.isupper():
+#                 cap+=1
+#             letter+=1
+# print(letter)
+# print('cap:',cap)
+# print('small:',letter-cap)
+'''14
+cap 3
+small 11'''
+f=open('./arya/python/demo.txt','r')
+l=f.readlines()
+f.seek(0)
+letter=0
+cap=0
+w=0
+for i in range(len(l)):
+    a=f.readline().strip()
+    s=a.split(' ')
+    for i in s:
+        if i!='':
+            w+=1
+    for i in a:
+         if i !=' ':
+            if i.isupper():
+                cap+=1
+            letter+=1
+print(letter)
+print('cap:',cap)
+print('small:',letter-cap)
+print('word:',w)
+print('no of lines:',len(l))
